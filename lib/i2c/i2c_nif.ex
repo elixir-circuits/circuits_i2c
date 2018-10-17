92f0e93de90fab0e07650c5f9cd82599a58dcc5d
Frank Hunleth <fhunleth@troodon-software.com>
2018-10-17 08:51:20 -0400

    Remove error message from failed NIF load
    It turns out that Erlang provides a good one already.

--- lib/i2c/i2c_nif.ex
+++ lib/i2c/i2c_nif.ex
@@ -2,24 +2,13 @@ defmodule ElixirCircuits.I2C.Nif do
   @on_load {:load_nif, 0}
   @compile {:autoload, false}
 
-  require Logger
-
   @doc """
   Elixir interface to I2C Natively Implemented Funtions (NIFs)
   """
 
   def load_nif() do
     nif_binary = Application.app_dir(:elixir_circuits_i2c, "priv/i2c_nif")
-
-    case :erlang.load_nif(to_charlist(nif_binary), 0) do
-      {:error, reason} ->
-        Logger.error(
-          "ElixirCircuits.I2CError: load_nif(#{nif_binary}) failed with #{inspect(reason)}"
-        )
-
-      _ ->
-        :ok
-    end
+    :erlang.load_nif(to_charlist(nif_binary), 0)
   end
 
   def open(_device, _address) do
