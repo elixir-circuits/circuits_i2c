d15cd4a1a30bc5f3831e16997eb19dbffc795b59
Frank Hunleth <fhunleth@troodon-software.com>
2018-11-06 08:58:02 -0500

    Add dialyxir so that we can check our specs

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule Circuits.I2C.MixProject do
   defp deps do
     [
       {:elixir_make, "~> 0.4", runtime: false},
-      {:ex_doc, "~> 0.11", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.11", only: :dev, runtime: false},
+      {:dialyxir, "1.0.0-rc.4", only: :dev, runtime: false}
     ]
   end
 
