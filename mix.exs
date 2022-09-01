ee433343ef38b57f79cd63847462a25a46219b9f
Frank Hunleth <fhunleth@troodon-software.com>
2022-09-01 09:46:11 -0600

    Remove unneeded build_embedded

--- mix.exs
+++ mix.exs
@@ -21,7 +21,6 @@ defmodule Circuits.I2C.MixProject do
       docs: docs(),
       aliases: [format: [&format_c/1, "format"]],
       start_permanent: Mix.env() == :prod,
-      build_embedded: true,
       dialyzer: [
         flags: [:unmatched_returns, :error_handling, :race_conditions, :underspecs]
       ],
