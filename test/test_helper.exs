c8fa4575205e87447eecb443be855c36e3cbe0f2
Frank Hunleth <fhunleth@troodon-software.com>
2022-11-16 10:43:51 -0500

    Remove unneeded Elixir <1.10 workaround

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,3 @@
-# Always warning as errors
-if Version.match?(System.version(), "~> 1.10") do
-  Code.put_compiler_option(:warnings_as_errors, true)
-end
+Code.put_compiler_option(:warnings_as_errors, true)
 
 ExUnit.start()
