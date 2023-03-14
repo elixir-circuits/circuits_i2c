1e0668b54bd9da77ed384e1df4aac31c7e051eba
Frank Hunleth <fhunleth@troodon-software.com>
2023-03-14 04:16:39 -0400

    Trim whitespace

--- lib/i2c/i2c_nif.ex
+++ lib/i2c/i2c_nif.ex
@@ -9,27 +9,13 @@ defmodule Circuits.I2C.Nif do
     :erlang.load_nif(to_charlist(nif_binary), 0)
   end
 
-  def open(_device) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def open(_device), do: :erlang.nif_error(:nif_not_loaded)
+  def read(_ref, _address, _count, _retries), do: :erlang.nif_error(:nif_not_loaded)
+  def write(_ref, _address, _data, _retries), do: :erlang.nif_error(:nif_not_loaded)
 
-  def read(_ref, _address, _count, _retries) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def write_read(_ref, _address, _write_data, _read_count, _retries),
+    do: :erlang.nif_error(:nif_not_loaded)
 
-  def write(_ref, _address, _data, _retries) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
-
-  def write_read(_ref, _address, _write_data, _read_count, _retries) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
-
-  def close(_ref) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
-
-  def info() do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def close(_ref), do: :erlang.nif_error(:nif_not_loaded)
+  def info(), do: :erlang.nif_error(:nif_not_loaded)
 end
