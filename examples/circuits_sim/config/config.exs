b06922e8833eadd1f4ba54121c198a5b04c29802
Jon Carstens <jjcarstens@me.com>
2023-03-22 22:57:24 -0600

    Add ADS7138 device

--- examples/circuits_sim/config/config.exs
+++ examples/circuits_sim/config/config.exs
@@ -4,7 +4,11 @@ import Config
 config :circuits_sim,
   config: %{
     "i2c-0" => %{0x20 => CircuitsSim.Device.MCP23008, 0x50 => CircuitsSim.Device.AT24C02},
-    "i2c-1" => %{0x20 => CircuitsSim.Device.MCP23008, 0x21 => CircuitsSim.Device.MCP23008}
+    "i2c-1" => %{
+      0x10 => CircuitsSim.Device.ADS7138,
+      0x20 => CircuitsSim.Device.MCP23008,
+      0x21 => CircuitsSim.Device.MCP23008
+    }
   }
 
 # Tell Circuits.I2C to default to simulated I2C buses
