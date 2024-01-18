13d6d3bfa9ef837963c4d23fac9c72cea3174587
Frank Hunleth <fhunleth@troodon-software.com>
2024-01-17 21:25:15 -0500

    Remove deferred loading of NIF
    It was possible to crash the BEAM by loading a completely trimmed down
    shared library with multiple processes. This converts the NIF loader
    back to the traditional `@on_load` way that's serialized by the code
    loader. The benefits of delaying the load time are less now that the
    shared library has been tested quite a bit.

--- lib/i2c/i2c_nif.ex
+++ lib/i2c/i2c_nif.ex
@@ -1,21 +1,14 @@
 defmodule Circuits.I2C.Nif do
   @moduledoc false
 
-  defp load_nif_and_apply(fun, args) do
-    nif_binary = Application.app_dir(:circuits_i2c, "priv/i2c_nif")
+  @on_load {:load_nif, 0}
+  @compile {:autoload, false}
 
-    # Optimistically load the NIF. Handle the possible race.
-    case :erlang.load_nif(to_charlist(nif_binary), 0) do
-      :ok -> apply(__MODULE__, fun, args)
-      {:error, {:reload, _}} -> apply(__MODULE__, fun, args)
-      error -> error
-    end
+  def load_nif() do
+    :erlang.load_nif(:code.priv_dir(:circuits_i2c) ++ ~c"/i2c_nif", 0)
   end
 
-  def open(device) do
-    load_nif_and_apply(:open, [device])
-  end
-
+  def open(_device), do: :erlang.nif_error(:nif_not_loaded)
   def read(_ref, _address, _count, _retries), do: :erlang.nif_error(:nif_not_loaded)
   def write(_ref, _address, _data, _retries), do: :erlang.nif_error(:nif_not_loaded)
 
@@ -23,8 +16,5 @@ defmodule Circuits.I2C.Nif do
     do: :erlang.nif_error(:nif_not_loaded)
 
   def close(_ref), do: :erlang.nif_error(:nif_not_loaded)
-
-  def info() do
-    load_nif_and_apply(:info, [])
-  end
+  def info(), do: :erlang.nif_error(:nif_not_loaded)
 end
